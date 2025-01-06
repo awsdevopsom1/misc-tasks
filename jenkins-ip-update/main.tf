a0f3eee70f5db1a9cc21f7a90d2725dac34fbd58
om <awsdevopsom@gmail.com>
2025-01-06 19:28:08 +0530

    om

--- jenkins-ip-update/main.tf
+++ jenkins-ip-update/main.tf
@@ -7,13 +7,30 @@ terraform {
 }
 
 data "aws_instance" "jenkins" {
-    instance_id = "i-0d59ffe290edf098e"
+    instance_id =  "i-0d59ffe290edf098e"
+  
 }
 
 resource "aws_route53_record" "jenkins-ip" {
-  zone_id = "Z0705553LI8IW1JSJIQR"
-  name    = "jenkins"
-  type    = "A"
-  ttl     = 10
-  records = [data.aws_instance.jenkins.public_ip]
+   zone_id = "Z0705553LI8IW1JSJIQR"
+   name = "jenkins"
+   type = "A"
+   ttl = 10
+   records = [data.aws_instance.jenkins.public_ip]
+  
+}
+
+
+data "aws_instance" "artifcatory" {
+    instance_id =  "i-0e131f0d3b5b82ca2"
+  
+}
+
+resource "aws_route53_record" "jenkins-ip" {
+   zone_id = "Z0705553LI8IW1JSJIQR"
+   name = "jenkins"
+   type = "A"
+   ttl = 10
+   records = [data.aws_instance.artifcatory.public_ip]
+
 }
